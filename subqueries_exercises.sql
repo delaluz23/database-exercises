749baef8d3361ee5565f5e451c9a6c7d13a87668
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-23 13:45:10 -0500

    added DISTICNT to title for unique titles

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,10 +1,11 @@
 use employees;
+
 select first_name, last_name
 from employees
 where hire_date in (select hire_date
                     from employees
                     where emp_no = 101010);
-select title
+select distinct title
 from titles
 where emp_no in (select emp_no
                  from employees
